c36f35c8e58dcf29b4128db59055b5a2b466e422
suayb <suayb.simsek@ykteknoloji.com.tr>
2025-03-18 23:21:43 +0300

    initial commit

--- src/main/kotlin/io/github/susimsek/springbootreactiveexample/enums/TodoSortField.kt
+++ src/main/kotlin/io/github/susimsek/springbootreactiveexample/enums/TodoSortField.kt
@@ -6,11 +6,13 @@ import io.swagger.v3.oas.annotations.media.Schema
     enumAsRef = true,
     description = "Sorting field:\n" +
         "* `CREATED_AT` - Sort by creation date\n" +
+        "* `UPDATED_AT` - Sort by update date\n" +
         "* `TITLE` - Sort by title\n" +
         "* `COMPLETED` - Sort by completion status\n"
 )
 enum class TodoSortField {
     CREATED_AT,
+    UPDATED_AT,
     TITLE,
     COMPLETED
 }
